317bb6eda8f3de00f585e25bc7a3ea60cbcaf195
Pedro Dorneles <pedroportzdorneles@gmail.com.br>
2025-12-09 10:20:49 -0300

    Salvando

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -26,7 +26,8 @@ export default withAuth(
                     pathname === "/" ||
                     pathname === "/login" ||
                     pathname === "/register" ||
-                    pathname.startsWith("/events/")
+                    pathname.startsWith("/events/") ||
+                    pathname.startsWith("/verify-certificate")
                 ) {
                     return true
                 }
